f92a8771e0c7190f7f3c5e8985f6abead8b0def1
GMX\Y9FSC9740 <mrobert3456@gmail.com>
2022-02-15 12:34:05 +0100

    ran migration file, which updated the scheme.rb

--- alpha-blog/db/schema.rb
+++ alpha-blog/db/schema.rb
@@ -10,10 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_15_105526) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_15_112512) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
